42c1d4366bb6d9b0b094cb966fcf6c820cc48457
abramsea <abramson.ea@gmail.com>
2019-10-30 14:47:58 +0300

    14:47

--- tabs.js
+++ tabs.js
@@ -6,15 +6,17 @@
         var d = document.getElementById('tab4');
         var e = document.getElementById('tab5');
         var f = document.getElementById('tab6');
+        var g = document.getElementById('tab11');
 
         if (a.className === "block b07") {
         a.className += " active";
         btn1.className += " selected";
-        b.className = "block b08"
-        c.className = "block b09"
-        d.className = "block b10"
-        e.className = "block b11"
-        f.className = "block b12"
+        b.className = "block b08";
+        c.className = "block b09";
+        d.className = "block b10";
+        e.className = "block b11";
+        f.className = "block b12";
+        g.className = "block b09";
         btn2.className = "nav_item";
         btn3.className = "nav_item";
         btn4.className = "nav_item";
@@ -31,20 +33,23 @@
         var d = document.getElementById('tab4');
         var e = document.getElementById('tab5');
         var f = document.getElementById('tab6');
+        var g = document.getElementById('tab11');
 
         if (b.className === "block b08") {
         a.className = "block b07";
-        b.className += " active"
+        b.className += " active";
         btn2.className += " selected";
-        c.className = "block b09"
-        d.className = "block b10"
-        e.className = "block b11"
-        f.className = "block b12"
+        c.className = "block b09";
+        d.className = "block b10";
+        e.className = "block b11";
+        f.className = "block b12";
+        g.className = "block b09";
         btn1.className = "nav_item";
         btn3.className = "nav_item";
         btn4.className = "nav_item";
         btn5.className = "nav_item";
         btn6.className = "nav_item";
+        btn7.className = "nav_item";
         }
     }
 
@@ -56,20 +61,23 @@
         var d = document.getElementById('tab4');
         var e = document.getElementById('tab5');
         var f = document.getElementById('tab6');
+        var g = document.getElementById('tab11');
 
         if (c.className === "block b09") {
         a.className = "block b07";
-        b.className = "block b08"
-        c.className += " active"
+        b.className = "block b08";
+        c.className += " active";
         btn3.className += " selected";
-        d.className = "block b10"
-        e.className = "block b11"
-        f.className = "block b12"
+        d.className = "block b10";
+        e.className = "block b11";
+        f.className = "block b12";
+        g.className = "block b09";
         btn2.className = "nav_item";
         btn1.className = "nav_item";
         btn4.className = "nav_item";
         btn5.className = "nav_item";
         btn6.className = "nav_item";
+        btn7.className = "nav_item";
         }
     }
 
@@ -80,20 +88,23 @@
         var d = document.getElementById('tab4');
         var e = document.getElementById('tab5');
         var f = document.getElementById('tab6');
+        var g = document.getElementById('tab11');
 
         if (d.className === "block b10") {
         a.className = "block b07";
-        b.className = "block b08"
-        c.className = "block b09"
-        d.className += " active"
+        b.className = "block b08";
+        c.className = "block b09";
+        d.className += " active";
         btn4.className += " selected";
-        e.className = "block b11"
-        f.className = "block b12"
+        e.className = "block b11";
+        f.className = "block b12";
+        g.className = "block b09";
         btn2.className = "nav_item";
         btn3.className = "nav_item";
         btn1.className = "nav_item";
         btn5.className = "nav_item";
         btn6.className = "nav_item";
+        btn7.className = "nav_item";
         }
     }
 
@@ -104,22 +115,26 @@
         var d = document.getElementById('tab4');
         var e = document.getElementById('tab5');
         var f = document.getElementById('tab6');
+        var g = document.getElementById('tab11');
 
         if (e.className === "block b11") {
         a.className = "block b07";
-        b.className = "block b08"
-        c.className = "block b09"
-        d.className = "block b10"
-        e.className += " active"
+        b.className = "block b08";
+        c.className = "block b09";
+        d.className = "block b10";
+        e.className += " active";
         btn5.className += " selected";
-        f.className = "block b12"
+        f.className = "block b12";
+        g.className = "block b09";
         btn2.className = "nav_item";
         btn3.className = "nav_item";
         btn4.className = "nav_item";
         btn1.className = "nav_item";
         btn6.className = "nav_item";
+        btn7.className = "nav_item";
         }
     }
+
     btn6.onclick = function showTab() {
         var a = document.getElementById('tab1');
         var b = document.getElementById('tab2');
@@ -127,42 +142,57 @@
         var d = document.getElementById('tab4');
         var e = document.getElementById('tab5');
         var f = document.getElementById('tab6');
+        var g = document.getElementById('tab11');
 
-        if (f.className === "block b12") {
+        if (g.className === "block b09") {
         a.className = "block b07";
-        b.className = "block b08"
-        c.className = "block b09"
-        d.className = "block b10"
-        e.className = "block b11"
-        f.className += " active"
+        b.className = "block b08";
+        c.className = "block b09";
+        d.className = "block b10";
+        e.className = "block b11";
+        f.className = "block b12";
+        g.className += " active";
         btn6.className += " selected";
         btn2.className = "nav_item";
         btn3.className = "nav_item";
         btn4.className = "nav_item";
         btn5.className = "nav_item";
+        btn7.className = "nav_item";
         btn1.className = "nav_item";
         }
     }
 
+
     btn7.onclick = function showTab() {
-        var a = document.getElementById('tab7');
-        var b = document.getElementById('tab8');
-        var c = document.getElementById('tab9');
-        var d = document.getElementById('tab10');
-    
+        var a = document.getElementById('tab1');
+        var b = document.getElementById('tab2');
+        var c = document.getElementById('tab3');
+        var d = document.getElementById('tab4');
+        var e = document.getElementById('tab5');
+        var f = document.getElementById('tab6');
+        var g = document.getElementById('tab11');
 
-        if (a.className === "block b07") {
-        a.className += " active"
-        b.className = "block b08"
-        c.className = "block b09"
-        d.className = "block b12"
-
+        if (f.className === "block b12") {
+        a.className = "block b07";
+        b.className = "block b08";
+        c.className = "block b09";
+        d.className = "block b10";
+        e.className = "block b11";
+        f.className += " active";
+        g.className = "block b09";
         btn7.className += " selected";
-        btn8.className = "nav_item";
-        btn9.className = "nav_item";
-        btn10.className = "nav_item";
+        btn2.className = "nav_item";
+        btn3.className = "nav_item";
+        btn4.className = "nav_item";
+        btn5.className = "nav_item";
+        btn6.className = "nav_item";
+        btn1.className = "nav_item";
         }
     }
+
+
+
+    // next button block 
 
 
     btn8.onclick = function showTab() {
@@ -170,18 +200,21 @@
         var b = document.getElementById('tab8');
         var c = document.getElementById('tab9');
         var d = document.getElementById('tab10');
+        var e = document.getElementById('tab12');
     
 
-        if (b.className === "block b08") {
-        a.className = "block b07"
-        b.className += " active"
-        c.className = "block b09"
-        d.className = "block b12"
+        if (a.className === "block b07") {
+        a.className += " active";
+        b.className = "block b08";
+        c.className = "block b09";
+        d.className = "block b12";
+        e.className = "block b09";
 
-        btn7.className = "nav_item";
         btn8.className += " selected";
         btn9.className = "nav_item";
         btn10.className = "nav_item";
+        btn11.className = "nav_item";
+        btn12.className = "nav_item";
         }
     }
 
@@ -191,18 +224,21 @@
         var b = document.getElementById('tab8');
         var c = document.getElementById('tab9');
         var d = document.getElementById('tab10');
+        var e = document.getElementById('tab12');
     
 
-        if (c.className === "block b09") {
-        a.className = "block b07"
-        b.className = "block b08"
-        c.className += " active"
-        d.className = "block b12"
+        if (b.className === "block b08") {
+        a.className = "block b07";
+        b.className += " active";
+        c.className = "block b09";
+        d.className = "block b12";
+        e.className = "block b09";
 
-        btn7.className = "nav_item";
         btn8.className = "nav_item";
         btn9.className += " selected";
         btn10.className = "nav_item";
+        btn11.className = "nav_item";
+        btn12.className = "nav_item";
         }
     }
 
@@ -212,17 +248,71 @@
         var b = document.getElementById('tab8');
         var c = document.getElementById('tab9');
         var d = document.getElementById('tab10');
+        var e = document.getElementById('tab12');
     
 
-        if (d.className === "block b12") {
-        a.className = "block b07"    
-        b.className = "block b08"
-        c.className = "block b09"
-        d.className += " active"
+        if (c.className === "block b09") {
+        a.className = "block b07";
+        b.className = "block b08";
+        c.className += " active";
+        d.className = "block b12";
+        e.className = "block b09";
 
-        btn7.className = "nav_item";
         btn8.className = "nav_item";
         btn9.className = "nav_item";
         btn10.className += " selected";
+        btn11.className = "nav_item";
+        btn12.className = "nav_item";
+        }
+    }
+
+    btn11.onclick = function showTab() {
+        var a = document.getElementById('tab7');
+        var b = document.getElementById('tab8');
+        var c = document.getElementById('tab9');
+        var d = document.getElementById('tab10');
+        var e = document.getElementById('tab12');
+    
+
+        if (e.className === "block b09") {
+        a.className = "block b07";    
+        b.className = "block b08";
+        c.className = "block b09";
+        d.className = "block b12";
+        e.className += " active";
+
+
+        btn8.className = "nav_item";
+        btn9.className = "nav_item";
+        btn10.className = "nav_item";
+        btn11.className += " selected";
+        btn12.className = "nav_item";
+ 
+        }
+    }
+
+    
+
+
+    btn12.onclick = function showTab() {
+        var a = document.getElementById('tab7');
+        var b = document.getElementById('tab8');
+        var c = document.getElementById('tab9');
+        var d = document.getElementById('tab10');
+        var e = document.getElementById('tab12');
+    
+
+        if (d.className === "block b12") {
+        a.className = "block b07";    
+        b.className = "block b08";
+        c.className = "block b09";
+        d.className += " active";
+        e.className = "block b09";
+
+        btn8.className = "nav_item";
+        btn9.className = "nav_item";
+        btn10.className = "nav_item";
+        btn11.className = "nav_item";
+        btn12.className += " selected";
         }
     }
