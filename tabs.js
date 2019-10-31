a68bcc3d044b8e01c7a8ef4ef73cab3e9f94554a
abramsea <abramson.ea@gmail.com>
2019-10-31 18:09:14 +0300

    js fix

--- tabs.js
+++ tabs.js
@@ -1,285 +1,195 @@
 
     btn1.onclick = function showTab() {
-        var a = document.getElementById('tab1');
-        var b = document.getElementById('tab2');
-        var c = document.getElementById('tab3');
-        var d = document.getElementById('tab4');
-        var e = document.getElementById('tab5');
-        var f = document.getElementById('tab6');
-        var g = document.getElementById('tab11');
+        tab1.className += " active";
+        tab2.className = "block b08";
+        tab3.className = "block b09";
+        tab4.className = "block b10";
+        tab5.className = "block b11";
+        tab6.className = "block b12";
+        tab11.className = "block b09";
 
-        if (a.className === "block b07") {
-        a.className += " active";
         btn1.className += " selected";
-        b.className = "block b08";
-        c.className = "block b09";
-        d.className = "block b10";
-        e.className = "block b11";
-        f.className = "block b12";
-        g.className = "block b09";
         btn2.className = "nav_item";
         btn3.className = "nav_item";
         btn4.className = "nav_item";
         btn5.className = "nav_item";
         btn6.className = "nav_item";
         }
-    }
 
 
     btn2.onclick = function showTab() {
-        var a = document.getElementById('tab1');
-        var b = document.getElementById('tab2');
-        var c = document.getElementById('tab3');
-        var d = document.getElementById('tab4');
-        var e = document.getElementById('tab5');
-        var f = document.getElementById('tab6');
-        var g = document.getElementById('tab11');
+        tab1.className = "block b07";
+        tab2.className += " active";
+        tab3.className = "block b09";
+        tab4.className = "block b10";
+        tab5.className = "block b11";
+        tab6.className = "block b12";
+        tab11.className = "block b09";
 
-        if (b.className === "block b08") {
-        a.className = "block b07";
-        b.className += " active";
-        btn2.className += " selected";
-        c.className = "block b09";
-        d.className = "block b10";
-        e.className = "block b11";
-        f.className = "block b12";
-        g.className = "block b09";
         btn1.className = "nav_item";
+        btn2.className += " selected";
         btn3.className = "nav_item";
         btn4.className = "nav_item";
         btn5.className = "nav_item";
         btn6.className = "nav_item";
         btn7.className = "nav_item";
-        }
+
     }
 
 
     btn3.onclick = function showTab() {
-        var a = document.getElementById('tab1');
-        var b = document.getElementById('tab2');
-        var c = document.getElementById('tab3');
-        var d = document.getElementById('tab4');
-        var e = document.getElementById('tab5');
-        var f = document.getElementById('tab6');
-        var g = document.getElementById('tab11');
+        tab1.className = "block b07";
+        tab2.className = "block b08";
+        tab3.className += " active";
+        tab4.className = "block b10";
+        tab5.className = "block b11";
+        tab6.className = "block b12";
+        tab11.className = "block b09";
 
-        if (c.className === "block b09") {
-        a.className = "block b07";
-        b.className = "block b08";
-        c.className += " active";
-        btn3.className += " selected";
-        d.className = "block b10";
-        e.className = "block b11";
-        f.className = "block b12";
-        g.className = "block b09";
-        btn2.className = "nav_item";
         btn1.className = "nav_item";
+        btn2.className = "nav_item";
+        btn3.className += " selected";
         btn4.className = "nav_item";
         btn5.className = "nav_item";
         btn6.className = "nav_item";
         btn7.className = "nav_item";
-        }
+
     }
 
     btn4.onclick = function showTab() {
-        var a = document.getElementById('tab1');
-        var b = document.getElementById('tab2');
-        var c = document.getElementById('tab3');
-        var d = document.getElementById('tab4');
-        var e = document.getElementById('tab5');
-        var f = document.getElementById('tab6');
-        var g = document.getElementById('tab11');
-
-        if (d.className === "block b10") {
-        a.className = "block b07";
-        b.className = "block b08";
-        c.className = "block b09";
-        d.className += " active";
-        btn4.className += " selected";
-        e.className = "block b11";
-        f.className = "block b12";
-        g.className = "block b09";
+        tab1.className = "block b07";
+        tab2.className = "block b08";
+        tab3.className = "block b09";
+        tab4.className += " active";
+        tab5.className = "block b11";
+        tab6.className = "block b12";
+        tab11.className = "block b09";
+        
+        btn1.className = "nav_item";
         btn2.className = "nav_item";
         btn3.className = "nav_item";
-        btn1.className = "nav_item";
+        btn4.className += " selected";
         btn5.className = "nav_item";
         btn6.className = "nav_item";
         btn7.className = "nav_item";
-        }
+
     }
 
     btn5.onclick = function showTab() {
-        var a = document.getElementById('tab1');
-        var b = document.getElementById('tab2');
-        var c = document.getElementById('tab3');
-        var d = document.getElementById('tab4');
-        var e = document.getElementById('tab5');
-        var f = document.getElementById('tab6');
-        var g = document.getElementById('tab11');
+        tab1.className = "block b07";
+        tab2.className = "block b08";
+        tab3.className = "block b09";
+        tab4.className = "block b10";
+        tab5.className += " active";
+        tab6.className = "block b12";
+        tab11.className = "block b09";
 
-        if (e.className === "block b11") {
-        a.className = "block b07";
-        b.className = "block b08";
-        c.className = "block b09";
-        d.className = "block b10";
-        e.className += " active";
-        btn5.className += " selected";
-        f.className = "block b12";
-        g.className = "block b09";
+        btn1.className = "nav_item";
         btn2.className = "nav_item";
         btn3.className = "nav_item";
         btn4.className = "nav_item";
-        btn1.className = "nav_item";
+        btn5.className += " selected";
         btn6.className = "nav_item";
         btn7.className = "nav_item";
-        }
     }
 
     btn6.onclick = function showTab() {
-        var a = document.getElementById('tab1');
-        var b = document.getElementById('tab2');
-        var c = document.getElementById('tab3');
-        var d = document.getElementById('tab4');
-        var e = document.getElementById('tab5');
-        var f = document.getElementById('tab6');
-        var g = document.getElementById('tab11');
-
-        if (g.className === "block b09") {
-        a.className = "block b07";
-        b.className = "block b08";
-        c.className = "block b09";
-        d.className = "block b10";
-        e.className = "block b11";
-        f.className = "block b12";
-        g.className += " active";
-        btn6.className += " selected";
+        tab1.className = "block b07";
+        tab2.className = "block b08";
+        tab3.className = "block b09";
+        tab4.className = "block b10";
+        tab5.className = "block b11";
+        tab6.className = "block b12";
+        tab11.className += " active";
+  
+        btn1.className = "nav_item";
         btn2.className = "nav_item";
         btn3.className = "nav_item";
         btn4.className = "nav_item";
         btn5.className = "nav_item";
+        btn6.className += " selected";
         btn7.className = "nav_item";
-        btn1.className = "nav_item";
-        }
+
+        
     }
 
 
     btn7.onclick = function showTab() {
-        var a = document.getElementById('tab1');
-        var b = document.getElementById('tab2');
-        var c = document.getElementById('tab3');
-        var d = document.getElementById('tab4');
-        var e = document.getElementById('tab5');
-        var f = document.getElementById('tab6');
-        var g = document.getElementById('tab11');
-
-        if (f.className === "block b12") {
-        a.className = "block b07";
-        b.className = "block b08";
-        c.className = "block b09";
-        d.className = "block b10";
-        e.className = "block b11";
-        f.className += " active";
-        g.className = "block b09";
-        btn7.className += " selected";
+        tab1.className = "block b07";
+        tab2.className = "block b08";
+        tab3.className = "block b09";
+        tab4.className = "block b10";
+        tab5.className = "block b11";
+        tab6.className += " active";
+        tab11.className = "block b09";
+ 
+        btn1.className = "nav_item";
         btn2.className = "nav_item";
         btn3.className = "nav_item";
         btn4.className = "nav_item";
         btn5.className = "nav_item";
         btn6.className = "nav_item";
-        btn1.className = "nav_item";
-        }
-    }
+        btn7.className += " selected";
 
+    }
 
 
     // next button block 
 
 
     btn8.onclick = function showTab() {
-        var a = document.getElementById('tab7');
-        var b = document.getElementById('tab8');
-        var c = document.getElementById('tab9');
-        var d = document.getElementById('tab10');
-        var e = document.getElementById('tab12');
-    
-
-        if (a.className === "block b07") {
-        a.className += " active";
-        b.className = "block b08";
-        c.className = "block b09";
-        d.className = "block b12";
-        e.className = "block b09";
-
+        tab7.className += " active";
+        tab8.className = "block b08";
+        tab9.className = "block b09";
+        tab10.className = "block b12";
+        tab12.className = "block b09";
+        
         btn8.className += " selected";
         btn9.className = "nav_item";
         btn10.className = "nav_item";
         btn11.className = "nav_item";
         btn12.className = "nav_item";
-        }
+     
     }
 
 
     btn9.onclick = function showTab() {
-        var a = document.getElementById('tab7');
-        var b = document.getElementById('tab8');
-        var c = document.getElementById('tab9');
-        var d = document.getElementById('tab10');
-        var e = document.getElementById('tab12');
-    
-
-        if (b.className === "block b08") {
-        a.className = "block b07";
-        b.className += " active";
-        c.className = "block b09";
-        d.className = "block b12";
-        e.className = "block b09";
-
+        tab7.className = "block b07";
+        tab8.className += " active";
+        tab9.className = "block b09";
+        tab10.className = "block b12";
+        tab12.className = "block b09";
+        
         btn8.className = "nav_item";
         btn9.className += " selected";
         btn10.className = "nav_item";
         btn11.className = "nav_item";
         btn12.className = "nav_item";
-        }
+    
     }
 
 
     btn10.onclick = function showTab() {
-        var a = document.getElementById('tab7');
-        var b = document.getElementById('tab8');
-        var c = document.getElementById('tab9');
-        var d = document.getElementById('tab10');
-        var e = document.getElementById('tab12');
-    
-
-        if (c.className === "block b09") {
-        a.className = "block b07";
-        b.className = "block b08";
-        c.className += " active";
-        d.className = "block b12";
-        e.className = "block b09";
+        tab7.className = "block b07";
+        tab8.className = "block b08";
+        tab9.className += " active";
+        tab10.className = "block b12";
+        tab12.className = "block b09";
 
         btn8.className = "nav_item";
         btn9.className = "nav_item";
         btn10.className += " selected";
         btn11.className = "nav_item";
         btn12.className = "nav_item";
-        }
+        
     }
 
     btn11.onclick = function showTab() {
-        var a = document.getElementById('tab7');
-        var b = document.getElementById('tab8');
-        var c = document.getElementById('tab9');
-        var d = document.getElementById('tab10');
-        var e = document.getElementById('tab12');
-    
-
-        if (e.className === "block b09") {
-        a.className = "block b07";    
-        b.className = "block b08";
-        c.className = "block b09";
-        d.className = "block b12";
-        e.className += " active";
+        tab7.className = "block b07";    
+        tab8.className = "block b08";
+        tab9.className = "block b09";
+        tab10.className = "block b12";
+        tab12.className += " active";
 
 
         btn8.className = "nav_item";
@@ -288,31 +198,23 @@
         btn11.className += " selected";
         btn12.className = "nav_item";
  
-        }
+    
     }
 
     
 
 
     btn12.onclick = function showTab() {
-        var a = document.getElementById('tab7');
-        var b = document.getElementById('tab8');
-        var c = document.getElementById('tab9');
-        var d = document.getElementById('tab10');
-        var e = document.getElementById('tab12');
-    
-
-        if (d.className === "block b12") {
-        a.className = "block b07";    
-        b.className = "block b08";
-        c.className = "block b09";
-        d.className += " active";
-        e.className = "block b09";
+        tab7.className = "block b07";    
+        tab8.className = "block b08";
+        tab9.className = "block b09";
+        tab10.className += " active";
+        tab12.className = "block b09";
 
         btn8.className = "nav_item";
         btn9.className = "nav_item";
         btn10.className = "nav_item";
         btn11.className = "nav_item";
         btn12.className += " selected";
-        }
+        
     }
